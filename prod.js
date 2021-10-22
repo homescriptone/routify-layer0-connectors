e1ae4b11e3c48dc9ef8a075df418337289d62399
Manu The Blacker <25031292+manutheblacker@users.noreply.github.com>
2021-10-22 11:44:30 +0100

    Add production dependencies

--- prod.js
+++ prod.js
@@ -4,5 +4,5 @@ module.exports = async function prod(port) {
     // Most frameworks export some kind of express server or other http listener. Your prod script
     // just needs to start it and bind it to the provided port.  In the case of Sapper, the way to do
     // this is to set the PORT environment variable and run the provided server script
-    require('./__sapper__/build/server/server')
+    require('@roxi/routify')
   }
