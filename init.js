5f2a8163e70abdd63c3fa5e4672f0c1e54a89cac
Manu The Blacker <25031292+manutheblacker@users.noreply.github.com>
2021-10-22 11:44:44 +0100

    Add path to app folder

--- init.js
+++ init.js
@@ -9,7 +9,7 @@ export default async function init() {
   new DeploymentBuilder(process.cwd())
     // Copy files from the default-app directory within the connector package.
     // These typically include the routes.js file and layer0.config.js. Typescript alternatives are often provided.
-    .addDefaultAppResources(join(__dirname, 'default-app'))
+    .addDefaultAppResources(join(__dirname, './src/'))
 
     // Adds layer0:* scripts to package.json
     .addDefaultLayer0Scripts()
